c77adc86b64ff061a076303bf364bb5a4a75fe06
twolakes <brian@twolakessolutions.com>
2022-12-26 22:45:43 -0500

    added player object

---
+++ player.py
@@ -0,0 +1,29 @@
+player = {
+    "name": "",
+    "session": {
+        "init_bank": 1000,
+        "curr_bank": 1000,
+        "sess_net": 0,
+        "sess_max": 0,
+        "rounds_pos": 0,
+        "sess_min": 0,
+        "rounds_neg": 0,
+        "spins": 0,
+        "wins": 0,
+        "losses": 0
+    },
+    "strategy": {
+        "base_u": 1,
+        "curr_u": 1,
+        "unit_mult": 10,
+        "bets": [],
+        "res_mod": {
+            "win_op": "mult",
+            "win_adj": 2,
+            "lose_op": "abs",
+            "lose_adj": 1
+        }
+    }
+
+
+}
